90b8260376df4c29eace58f6e11a3134357e5f79
goleador0322 <goleador0322@gmail.com>
2017-10-25 21:29:27 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -11,17 +11,16 @@ if os.path.exists('README.rst'):
     long_description = codecs.open('README.rst', encoding='utf-8').read()
 
 setup(
-    name='summpy',
-    version='0.2.1',
+    name='summpy-fix',
+    version='1.0.0',
     description='Text summarization (sentence extraction) module with simple HTTP API. (currently, supports Japanese only)',
     long_description=long_description,
-    author='Shumpei IINUMA',
-    author_email='shumpei.iinuma@gmail.com',
+    author='OnetapInc',
+    author_email='onetap0507@gmail.com',
     license='MIT',
-    url='https://github.com/recruit-tech/summpy',
-    #download_url='https://github.com/recruit-tech/summpy/tarball/0.2',
-    packages=['summpy', 'summpy.misc'],
-    package_data={'summpy': ['server_data/*.html']},
+    url='https://github.com/OnetapInc/summpy-fix',
+    packages=['summpy-fix', 'summpy.misc'],
+    package_data={'summpy-fix': ['server_data/*.html']},
     install_requires=[
         'numpy', 'scipy', 'scikit-learn', 'networkx', 'cherrypy'
     ],
